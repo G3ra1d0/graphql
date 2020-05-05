b7c728e6281574f30e50b6c930ff481866940888
G3ra1d0 <geraldo-vitor@hotmail.com>
2020-05-05 11:19:35 -0300

    add middleware token no context

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 const { ApolloServer, gql } = require("apollo-server");
 const { importSchema } = require("graphql-import");
 const resolvers = require("./resolvers");
+const { verify } = require("./middleware/auth/jwt");
+
 require("./db");
 
 const server = new ApolloServer({
   typeDefs: importSchema("./schema/index.graphql"),
   resolvers,
+  context: ({ req }) => {
+    const usuario = null;
+    const auth = req.headers.authorization;
+    if (auth) {
+      const token = auth.split(" ")[1];
+      if (token) {
+        try {
+          let decoded = verify(token);
+          usuario = decoded;
+        } catch (err) {
+          // console.log(err);
+        }
+      }
+    }
+
+    return usuario;
+  },
 });
 
 server.listen().then(({ url }) => {
